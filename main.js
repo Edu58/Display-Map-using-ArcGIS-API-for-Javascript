de025a2651cc59aaf9f05ef277527214b655c20b
Edu58 <edumuriithi58@gmail.com>
2021-12-20 15:30:40 +0300

    first-commit

--- main.js
+++ main.js
@@ -1,23 +1,29 @@
 const API_KEY =
     "AAPKf2b8d3e6dc294dd284c1c7cd64ca81acP3m2qEpZ1nXLbIrKfvRuT-kQ76E56-z8umEeb5EXfxA2kRWmu5NTAoJV9FZfgCCR";
-  
-const div = document.getElementById("Map")
 
 require([
     'esri/config',
     'esri/Map',
-    'esri/views/MapView'
-], function (esriConfig, Map, MapView) {
+    'esri/views/MapView',
+    'esri/widgets/BasemapToggle'
+], function (esriConfig, Map, MapView, BasemapToggle) {
     esriConfig.apikey = API_KEY;
 
     const map = new Map({
-        basemap: "dark-gray-vector"
+        basemap: 'arcgis-light-gray'
     });
 
     const view = new MapView({
       map: map,
-      center: [-118.805, 34.027],
-      zoom: 10,
-      container: div,
+      center: [36.81667, -1.28333],
+        container: "Map",
+      zoom: 10
     });
+
+    const baseMapToggle = new BasemapToggle({
+        view: view,
+        nextBasemap: "arcgis-streets-night"
+    });
+
+    view.ui.add(baseMapToggle, "bottom-right");
 })
