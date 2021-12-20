32f4ea081d08907b65247fb3119e5301037eb0f4
Edwin Karimi <edumuriithi58@gmail.com>
2021-12-20 15:58:18 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
-const API_KEY =
-    "AAPKf2b8d3e6dc294dd284c1c7cd64ca81acP3m2qEpZ1nXLbIrKfvRuT-kQ76E56-z8umEeb5EXfxA2kRWmu5NTAoJV9FZfgCCR";
+const API_KEY = "AAPKf2b8d3e6dc294dd284c1c7cd64ca81acP3m2qEpZ1nXLbIrKfvRuT-kQ76E56-z8umEeb5EXfxA2kRWmu5NTAoJV9FZfgCCR";
 
 require([
     'esri/config',
     'esri/Map',
-    'esri/views/MapView',
-], function (esriConfig, Map, MapView) {
-    esriConfig.apikey = API_KEY;
+    'esri/views/MapView'
+],
 
-    const map = new Map({
-        basemap: 'arcgis-light-gray'
-    });
+    function (esriConfig, Map, MapView) {
+        esriConfig.apikey = API_KEY;
 
-    const view = new MapView({
-      map: map,
-      center: [36.81667, -1.28333],
-        container: "Map",
-      zoom: 10
-    });
-})
+        const map = new Map({
+          basemap: "osm-standard-relief", //change this into the type of basemap you want
+        });
+
+        const view = new MapView({
+          container: "MAP-AREA", //div id where map is loaded
+          map: map,
+          center: [-118.805, 34.027], //longitude and latitude
+          zoom: 13, //zoom level
+        });
+    }
+)
